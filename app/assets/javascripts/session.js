775beb8b2a32255016fd5ea3fb689701f3c991c8
Emilia Kubo Kirschenbaum <ekubokirs@gmail.com>
2013-07-20 13:32:50 -0700

    finished show/hide button

--- app/assets/javascripts/session.js
+++ app/assets/javascripts/session.js
@@ -1,16 +1,14 @@
 $(function(){
 	$('#toggle-password-field').on('click', function(evt){
-		
 		if ($(this).hasClass('alert')){
-
 			$(this).html('Hide Password');
 
 			$(this).removeClass('alert');
 			$(this).addClass('success');
 
-			$('#password').attr('type', 'text');
+			$('#inputPassword').attr('type','text')
 		}
-		
+
 		else{
 
 			$(this).html('Show Password');
@@ -19,12 +17,9 @@ $(function(){
 			$(this).removeClass('success');
 			$(this).addClass('alert');
 
-			$('#password').attr('type', 'password');
+			$('#inputPassword').attr('type','password')
 		}
 	
 		return false;
-	})
-
-
-	
+	});
 })
